22d39f3c4e97984a90b4b8051ab871ffda0e5b84
Olya Virchenko <olyavirchenko@MacBook-Pro-Olya.local>
2025-06-30 19:05:52 +0300

    age on stage

---
+++ src/Age_Stage.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Age_Stage {
+
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner sc = new Scanner(System.in);
+
+        // Read the integer
+        int age = sc.nextInt();
+
+        if (age > 0 && age < 13) {
+            System.out.println("Child");
+        } else if (age > 12 && age < 18) {
+            System.out.println("Teenager");
+        } else if (age > 17 && age < 60) {
+            System.out.println("Adult");
+        } else if (age > 59 && age <= 100) {
+            System.out.println("Senior Citizen");
+        }
+
+        sc.close();
+    }
+}
